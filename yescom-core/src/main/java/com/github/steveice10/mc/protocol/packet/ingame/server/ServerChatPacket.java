db0111e5b0137b684b1983dddfcebad81e9e7e61
node3112 <node3113@gmail.com>
2022-05-22 02:08:13 +0100

    Patched annoying chat message bug (I HATE gradle).

---
+++ yescom-core/src/main/java/com/github/steveice10/mc/protocol/packet/ingame/server/ServerChatPacket.java
@@ -0,0 +1,65 @@
+package com.github.steveice10.mc.protocol.packet.ingame.server;
+
+import com.github.steveice10.mc.protocol.data.MagicValues;
+import com.github.steveice10.mc.protocol.data.game.MessageType;
+import com.github.steveice10.mc.protocol.data.message.Message;
+import com.github.steveice10.mc.protocol.packet.MinecraftPacket;
+import com.github.steveice10.packetlib.io.NetInput;
+import com.github.steveice10.packetlib.io.NetOutput;
+
+import java.io.IOException;
+
+/**
+ * Dang.
+ */
+public class ServerChatPacket extends MinecraftPacket {
+    private String messageText;
+    private Message message;
+    private MessageType type;
+
+    @SuppressWarnings("unused")
+    private ServerChatPacket() {
+    }
+
+    public ServerChatPacket(String text) {
+        this(Message.fromString(text));
+    }
+
+    public ServerChatPacket(Message message) {
+        this(message, MessageType.SYSTEM);
+    }
+
+    public ServerChatPacket(String text, MessageType type) {
+        this(Message.fromString(text), type);
+    }
+
+    public ServerChatPacket(Message message, MessageType type) {
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public Message getMessage() {
+        return this.message;
+    }
+
+    public MessageType getType() {
+        return this.type;
+    }
+
+    @Override
+    public void read(NetInput in) throws IOException {
+        messageText = in.readString();
+        this.message = Message.fromString(messageText);
+        this.type = MagicValues.key(MessageType.class, in.readByte());
+    }
+
+    @Override
+    public void write(NetOutput out) throws IOException {
+        out.writeString(this.message.toJsonString());
+        out.writeByte(MagicValues.value(Integer.class, this.type));
+    }
+}
