7f4e9a003c2d4c2e26e80e17b3d7f60e8651c5d8
node3112 <node3113@gmail.com>
2022-06-11 18:05:12 +0100

    Native extraction.

--- yescom-ui/src/main/java/ez/pogdog/yescom/ui/Main.java
+++ yescom-ui/src/main/java/ez/pogdog/yescom/ui/Main.java
@@ -5,18 +5,35 @@ import ez.pogdog.yescom.core.util.Bootstrap;
 import jep.Interpreter;
 import jep.MainInterpreter;
 import jep.PyConfig;
+import jep.Run;
 import jep.SharedInterpreter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Main UI class.
  */
 public class Main {
 
+    private static boolean deleteDirectory(File directory) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) deleteDirectory(file);
+        }
+        return directory.delete();
+    }
+
     public static void main(String[] args) {
         Logger logger = Logging.getLogger("yescom.ui");
+        logger.setLevel(Level.ALL); // FIXME: Remove, just testing
 
         logger.fine("Bootstrapping jep...");
         PyConfig config = new PyConfig();
@@ -26,16 +43,49 @@ public class Main {
         logger.info("Starting YesCom UI...");
 
         Interpreter interpreter = new SharedInterpreter();
-        interpreter.exec("from sys import path");
+
         String jarPath = "";
+        File yesComNatives = null;
         try {
             jarPath = Bootstrap.findJar();
+            yesComNatives = new File(System.getProperty("java.io.tmpdir"), "yescom-natives");
+            if (!yesComNatives.exists() && !yesComNatives.mkdirs()) throw new IOException("Couldn't create temp dir.");
+
+            ZipFile thisFile = new ZipFile(jarPath);
+            Enumeration<? extends ZipEntry> entries = thisFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                if (!entry.isDirectory() && entry.getName().startsWith("yescom/")) {
+                    File outputFile = new File(yesComNatives, entry.getName());
+                    logger.fine(String.format("Extracting %s -> %s", entry.getName(), outputFile.getName()));
+
+                    if (!outputFile.exists()) {
+                        File parent = outputFile.getParentFile();
+                        if (!parent.isDirectory() && !parent.mkdirs()) throw new IOException("Couldn't create output directory.");
+                        if (!outputFile.createNewFile()) throw new IOException("Couldn't create output file.");
+                    }
+
+                    FileOutputStream outputStream = new FileOutputStream(outputFile);
+                    thisFile.getInputStream(entry).transferTo(outputStream);
+                    outputStream.close();
+                }
+            }
+            thisFile.close();
+
+            File finalYesComNatives = yesComNatives;
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                logger.info("Cleaning up...");
+                if (finalYesComNatives.exists()) deleteDirectory(finalYesComNatives);
+            }));
+
         } catch (Exception error) {
             logger.severe("Couldn't start YesCom: " + error.getMessage());
             logger.throwing(Main.class.getSimpleName(), "main", error);
             System.exit(1);
         }
-        interpreter.invoke("path.append", jarPath);
+        interpreter.exec("from sys import path");
+        interpreter.invoke("path.append", yesComNatives.getAbsolutePath());
+        // interpreter.invoke("path.append", jarPath);
 
         // YesCom.getInstance().start();
 
